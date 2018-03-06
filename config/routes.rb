5338db320c1d299e4d425d7a879d8d965ed0ca9c
Paul Ehrhardt <paul.ehrhardt@hec.edu>
2018-03-06 15:33:39 +0100

    my second rails app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Read all
+  get '/tasks', to: 'tasks#index'
+
+  # Read 1
+  get '/tasks/new', to: 'tasks#new'
+
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
+
+  # Create new
+  post '/tasks', to: 'tasks#create'
+
+  # Edit
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+
+  # Update
+  patch '/tasks/:id', to: 'tasks#update'
+
+  # Remove
+  delete '/tasks/:id', to: 'tasks#destroy'
+
+  # resources :tasks
 end
